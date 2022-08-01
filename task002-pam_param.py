74d8692c4087a34a8ae99d004c70992d21ef80c5
Dmitriy_Klaes <89119584059@mail.ru>
2022-08-01 20:56:30 +0300

    Добавил решения от преподавателя

--- task002-pam_param.py
+++ task002-pam_param.py
@@ -11,3 +11,22 @@ def sense_of_rhythm(chant):
         
         
 sense_of_rhythm(input('Введите кричалку: ').split())
+
+
+# от препода
+
+def check(text):
+    sets = set()
+    for i in text.split():
+        count = 0
+        for j in i:
+            if j in 'ауоыиэяюёе':
+                count += 1
+        sets.add(count)
+    if len(sets) == 1:
+        return 'yes'
+    return 'no'
+
+
+text = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
+print(check(text))
